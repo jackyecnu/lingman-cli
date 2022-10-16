b12edc8bda1c303c874187e947a89e65084c26f1
jackyzhong <jackyecnu@126.com>
2022-10-16 15:23:48 +0800

    chore: release v1.0.16

--- src/open/index.ts
+++ src/open/index.ts
@@ -3,15 +3,24 @@ import chalk from 'chalk'
 
 export function openDocs({ docs }) {
   if (!docs) return console.log(chalk.red('缺少docs配置'))
-  execSync(`start ${docs}`, { stdio: 'inherit' })
+  openInBrowser(docs)
 }
 
 export function openDocs1({ docs1 }) {
   if (!docs1) return console.log(chalk.red('缺少docs1配置'))
-  execSync(`start ${docs1}`, { stdio: 'inherit' })
+  openInBrowser(docs1)
 }
 
 export function openLog({ log }) {
   if (!log) return console.log(chalk.red('缺少log地址配置'))
-  execSync(`start ${log}`, { stdio: 'inherit' })
+  openInBrowser(log)
 }
+
+function openInBrowser(url) {
+  const isWin = process.platform === 'win32'
+  if (isWin)
+    execSync(`start '${url}'`, { stdio: 'inherit', shell: 'powershell.exe' })
+  else
+    execSync(`open '${url}'`)
+}
+

0eb4b38911a7571236c26b085eb66fe7419d3a97
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-10-16 15:20:48 +0800

    chore: release v1.0.15

--- src/sync/index.ts
+++ src/sync/index.ts
@@ -3,6 +3,7 @@ import chalk from 'chalk'
 
 export function sync({ sync }) {
   if (!sync) return console.log(chalk.red('缺少sync配置'))
+
   const isWin = process.platform === 'win32'
   if (isWin)
     execSync(sync, { stdio: 'inherit', shell: 'powershell.exe' })

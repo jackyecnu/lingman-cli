c531cd9c5b86de78e74f294eaf9aaac76a087db5
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-10-17 09:40:51 +0800

    chore: release v1.0.19

--- src/init-config/index.ts
+++ src/init-config/index.ts
@@ -1,11 +1,15 @@
 import path from 'path'
 import fs from 'fs'
 import axios from 'axios'
+import chalk from 'chalk'
 
 export function initConfig({ url, path: configPath }) {
-  const targetPath = path.resolve(process.cwd(), configPath)
+  if (!url) return console.log(chalk.red('缺少initConfig.url配置'))
+  if (!configPath) return console.log(chalk.red('缺少initConfig.path配置'))
+
+  const fileName = path.basename(url)
+  const targetPath = path.resolve(process.cwd(), configPath, fileName)
   axios.get(url).then(({ data }) => {
-    // 写入配置文件
     fs.writeFileSync(targetPath, data, { encoding: 'utf-8' })
   })
 }

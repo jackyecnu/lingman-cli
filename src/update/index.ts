6a087a7688ad4032e757c216d3430debfa98a750
jackyzhong <jackyecnu@126.com>
2022-10-17 19:26:12 +0800

    chore: release v1.0.25

--- src/update/index.ts
+++ src/update/index.ts
@@ -2,5 +2,6 @@ import { execSync } from 'child_process'
 
 export function update() {
   execSync('npm install -g lingman-pub', { stdio: 'inherit' })
+  execSync('lm -v', { stdio: 'inherit' })
   console.log('淘宝源同步： https://www.npmmirror.com/package/lingman-pub')
 }

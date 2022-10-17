7bbb299cceb458917f9334ebb4fe07e9136532bd
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-10-17 09:34:47 +0800

    refactor

--- src/update/index.ts
+++
@@ -1,6 +0,0 @@
-import { execSync } from 'child_process'
-
-export function update() {
-  execSync('npm install -g lingman-pub', { stdio: 'inherit' })
-  console.log('淘宝源同步： https://www.npmmirror.com/package/lingman-pub')
-}

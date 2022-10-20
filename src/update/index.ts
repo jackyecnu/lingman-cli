381582431373082fd75c1063436ece6411248d59
Jacky 钟双金 <jackyecnu@126.com>
2022-10-21 07:27:59 +0800

    chore: release v1.0.36

--- src/update/index.ts
+++ src/update/index.ts
@@ -5,6 +5,6 @@ export function update() {
   execSync('pnpm install -g lingman-pub', { stdio: 'inherit' })
   execSync('lm -v', { stdio: 'inherit' })
   console.log('当前源:如果为淘宝源，则运行 lm taobao 去同步')
-  execSync('npm get registry')
+  execSync('npm get registry', { stdio: 'inherit' })
 
 }

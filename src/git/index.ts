2091a0279b0a188d43013987d49d0ea9d181c272
jackyzhong <jackyecnu@126.com>
2022-10-31 19:15:33 +0800

    提交

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,11 +1,11 @@
 import { execSync } from 'child_process'
 
 export async function gitPush(message, args: string[]) {
-  await execSync('nothing to commit', { stdio: 'inherit' })
   // if (res.toString().includes('nothing to commit'))
   //   console.log(`错误：${chalk.yellow('无更新')}`)
   try {
     execSync('git add .', { stdio: 'inherit' })
+    console.log('output========gg')
     if (args[0] === 'no')
       execSync('git commit -m "nobuild"', { stdio: 'inherit' })
     else

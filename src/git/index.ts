1892bd38b932cbacea300f2924dfc38b3f550414
jackyzhong <jackyecnu@126.com>
2022-10-31 19:07:04 +0800

    chore: release v1.0.57

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,13 +1,10 @@
 import { execSync } from 'child_process'
-import chalk from 'chalk'
 
-export function gitPush(message) {
-  const res = execSync('nothing to commit', { stdio: 'inherit' })
-  if (res.includes('nothing to commit'))
-    console.log(`错误：${chalk.yellow('无更新')}`)
-
+export async function gitPush(message) {
+  await execSync('nothing to commit', { stdio: 'inherit' })
+  // if (res.toString().includes('nothing to commit'))
+  //   console.log(`错误：${chalk.yellow('无更新')}`)
   execSync('git add .', { stdio: 'inherit' })
-
   if (message === 'no')
     execSync('git commit -m "nobuild"', { stdio: 'inherit' })
   else

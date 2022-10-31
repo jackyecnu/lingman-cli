897e2b7286fd15b29ff59bc4a5ad221da622bef6
jackyzhong <jackyecnu@126.com>
2022-10-31 15:46:25 +0800

    chore: release v1.0.55

--- src/utils/checkVersion.ts
+++ src/utils/checkVersion.ts
@@ -5,5 +5,5 @@ import { version } from '../../package.json'
 export function checkVersion() {
   const latest_version = execSync('npm view lingman-pub version', { stdio: 'pipe' }).toString().trim()
   if (latest_version !== version)
-    console.log(`有新版本更新啦! 输入: ${chalk.yellow('npm i -g lingman-pub')} 更新到最新版`)
+    console.log(`有新版本更新啦! 输入: ${chalk.yellow('lm update')} 更新到最新版`)
 }

e5b2db1cb782de2386a4d07a066d163640fa90b2
jackyzhong <jackyecnu@126.com>
2022-10-31 19:18:47 +0800

    提交

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,20 +1,19 @@
 import { execSync } from 'child_process'
+import chalk from 'chalk'
 
 export async function gitPush(message, args: string[]) {
   // if (res.toString().includes('nothing to commit'))
   //   console.log(`错误：${chalk.yellow('无更新')}`)
   try {
     execSync('git add .', { stdio: 'inherit' })
-    console.log('output========gg')
+
     if (args[0] === 'no')
       execSync('git commit -m "nobuild"', { stdio: 'inherit' })
     else
       execSync(`git commit -m "${message || '提交'}"`, { stdio: 'inherit' })
     execSync('git push', { stdio: 'inherit' })
-    console.log('output========ss')
   }
   catch (err) {
-    console.log('output========', err)
-    // console.log('sdterr', err.stderr.toString())
+    console.log(`${chalk.red('运行出错')}`)
   }
 }

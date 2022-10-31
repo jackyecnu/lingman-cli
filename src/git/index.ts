b4e0ecb2742bcaf81d531c6c4ce7eef8f7653dbe
jackyzhong <jackyecnu@126.com>
2022-10-31 17:49:52 +0800

    ggg

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,10 +1,10 @@
 import { execSync } from 'child_process'
 
-export function gitPush(message, args: string[]) {
+export function gitPush(message) {
   execSync('git add .', { stdio: 'inherit' })
-  if (args.length === 0)
-    execSync(`git commit -m "${message || '提交'}"`, { stdio: 'inherit' })
-  else if (args[0] === 'no')
+  if (message === 'no')
     execSync('git commit -m "nobuild"', { stdio: 'inherit' })
+  else
+    execSync(`git commit -m "${message || '提交'}"`, { stdio: 'inherit' })
   execSync('git push', { stdio: 'inherit' })
 }
--- src/lang.ts
+++ src/lang.ts
@@ -31,7 +31,9 @@ export function langCommon(program, config) {
     .description('git提交 默认提交工作区所有文件')
     .option('-m, --message <message>', '提交信息')
     .action((options) => {
-      gitPush(options.message, program.args.slice(1))
+      // console.log(options.message, 'aaaa', program.args.slice(1))
+      console.dir(options)
+      gitPush(options.message)
     })
 
   program

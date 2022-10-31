0fe42b00e8f50aa89e6b10887e381f5189a7e0cb
jackyzhong <jackyecnu@126.com>
2022-10-31 17:54:40 +0800

    提交

--- src/git/index.ts
+++ src/git/index.ts
@@ -2,6 +2,7 @@ import { execSync } from 'child_process'
 
 export function gitPush(message) {
   execSync('git add .', { stdio: 'inherit' })
+
   if (message === 'no')
     execSync('git commit -m "nobuild"', { stdio: 'inherit' })
   else

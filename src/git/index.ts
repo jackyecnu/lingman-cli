28d98db249243b0dd65116795684d41524322441
jackyzhong <jackyecnu@126.com>
2022-10-31 17:57:44 +0800

    提交

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,6 +1,11 @@
 import { execSync } from 'child_process'
+import chalk from 'chalk'
 
 export function gitPush(message) {
+  const res = execSync('nothing to commit', { stdio: 'inherit' })
+  if (res.includes('nothing to commit'))
+    console.log(`错误：${chalk.yellow('无更新')}`)
+
   execSync('git add .', { stdio: 'inherit' })
 
   if (message === 'no')

6d184f87a3d04e21afce003afbef72b5c2c69007
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-11-01 11:31:12 +0800

    chore: release v1.0.60

--- src/utils/checkVersion.ts
+++ src/utils/checkVersion.ts
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { version } from '../../package.json'
 
 export function checkVersion() {
-  const latest_version = execSync('npm view lingman-pub version', { stdio: 'pipe' }).toString().trim()
+  const latest_version = execSync('pnpm view lingman-pub version --registry https://registry.npmmirror.com', { stdio: 'pipe' }).toString().trim()
   if (latest_version !== version)
     console.log(`有新版本更新啦! 输入: ${chalk.yellow('lm update')} 更新到最新版`)
 }

ee28312ba5ba936f617d732318d1b4a620f6d1a2
jackyzhong <jackyecnu@126.com>
2022-11-04 10:16:17 +0800

    chore: release v1.0.68

--- src/utils/checkVersion.ts
+++ src/utils/checkVersion.ts
@@ -5,5 +5,5 @@ import { version } from '../../package.json'
 export function checkVersion() {
   const latest_version = execSync('pnpm view lingman-pub version --registry https://registry.npmmirror.com', { stdio: 'pipe' }).toString().trim()
   if (latest_version !== version)
-    console.log(`有新版本更新啦! 输入: ${chalk.bold.yellow('lm update')} 更新到最新版`)
+    console.log(`有新版本更新啦! 输入: ${chalk.bold.yellow('lm u')} 更新到最新版`)
 }

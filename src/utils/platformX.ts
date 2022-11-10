a3f89343e812176ea7ba401e4d662d3b5ff473fa
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-11-10 09:27:15 +0800

    提交

--- src/utils/platformX.ts
+++ src/utils/platformX.ts
@@ -1,6 +1,9 @@
-export async function PlatformX() {
+export class PlatformX {
+  static get isWin() {
+    return process.platform === 'win32'
+  }
 
+  static get isMac() {
+    return process.platform === 'darwin'
+  }
 }
-
-PlatformX.isWin = process.platform === 'win32'
-PlatformX.isMac = process.platform === 'darwin'

c44efb01c43e55ed316cfa4415442bbb0e31a018
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-01-30 19:39:41 +0800

    修复: lm lingman

--- src/langs/flutter/lingman/index.ts
+++ src/langs/flutter/lingman/index.ts
@@ -7,6 +7,7 @@ import { runCmd } from '../../../common/runcmd'
 export async function updateLingmanVersionForFlutter() {
   const res = await axios.get('https://pub.flutter-io.cn/api/documentation/flutter_lingman')
   const { latestStableVersion } = res.data
+  console.log(chalk.bold.yellow(`最新版本: ${latestStableVersion}`))
 
   const pubspecPath = path.resolve(process.cwd(), 'pubspec.yaml')
 
@@ -15,9 +16,10 @@ export async function updateLingmanVersionForFlutter() {
   const reg = /flutter_lingman:\s+\^?(.*)[\r\n]+/
 
   if (!reg.test(pubspec)) {
-    const newPubspec = pubspec.replace('dependencies:\r\n', `dependencies:\r\n  flutter_lingman: ${latestStableVersion}\r\n\r\n`)
-    fs.writeFileSync(pubspecPath, newPubspec)
-    runCmd('flutter pub get')
+    // const newPubspec = pubspec.replace('dependencies:\r\n', `dependencies:\r\n  flutter_lingman: ${latestStableVersion}\r\n\r\n`)
+    // fs.writeFileSync(pubspecPath, newPubspec)
+    // runCmd('flutter pub get')
+    runCmd('flutter pub add flutter_lingman')
     console.log(chalk.bold.green('更新成功'))
 
     return

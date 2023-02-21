3a6f5997c7afed60c856e91918cc9576400cce4b
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-02-21 10:07:24 +0800

    其他: 修改 cmd 逻辑

--- src/common/runcmd/index.ts
+++ src/common/runcmd/index.ts
@@ -18,14 +18,17 @@ export async function runCmd(cmd, win = 'powershell') {
 
 export async function getCommand() {
   const res = await axios.get(`https://api-lmapp.lingman.tech/api/app/devtool/command/all/${process.platform}`)
+
   const choose = await inquirer.prompt([
     {
       type: 'rawlist',
       message: '请选择运行命令 ?',
       name: 'type',
-      choices: res.data.data.map(item => ({ name: item.name, value: item.content })),
+      choices: res.data.data.map(item => ({ name: item.name, value: item })),
     },
   ])
 
-  runCmd(choose.type)
+  console.log(chalk.green.bold(`执行指令: ${choose.type.content}`))
+  if (choose.type.is_run === 1)
+    runCmd(choose.type.content)
 }

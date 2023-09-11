7828507e789a4b4999e7c0ea7905c61881d5f868
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-09-11 09:26:46 +0800

    chore: release v1.0.143

--- src/common/update/index.ts
+++ src/common/update/index.ts
@@ -2,8 +2,8 @@ import { execSync } from 'node:child_process'
 
 export function update() {
   execSync('lm -v', { stdio: 'inherit' })
-  execSync('npm install -g lingman-cli --registry https://registry.npmmirror.com', { stdio: 'inherit' })
+  execSync('pnpm install -g lingman-cli --registry https://registry.npmmirror.com', { stdio: 'inherit' })
   execSync('lm -v', { stdio: 'inherit' })
   console.log('当前源:如果为淘宝源，则运行 lm taobao 去同步')
-  execSync('npm get registry', { stdio: 'inherit' })
+  execSync('pnpm get registry', { stdio: 'inherit' })
 }
